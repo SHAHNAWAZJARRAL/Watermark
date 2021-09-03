3fa4791a9c74a280c05e9aca4d45dbe16eb32074
shahn <shahn@DESKTOP-9QMUG3K>
2021-09-03 23:34:07 +0500

    Add project files.

---
+++ addWaterMark/addMark.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addWaterMark
+{
+    public class addMark
+    {
+        public void watermark()
+        {
+            //Parallel.ForEach(Directory.GetFiles(@"" + Environment.CurrentDirectory + "\\img"), new ParallelOptions() { MaxDegreeOfParallelism = 2 }, img =>
+            //{
+            //    Image baseimg = Image.FromFile(img);
+            //    Graphics graphics = Graphics.FromImage(baseimg);
+            //    Image watermark = Image.FromFile(@"" + Environment.CurrentDirectory + "\\watermark\\" + markName + ".png");
+            //    decimal de_w = (decimal)baseimg.Width / watermark.Width * 100;
+            //    int i_w = (int)de_w;
+            //    var resized = new Bitmap(watermark, (watermark.Width * i_w) / 100, (watermark.Height * i_w) / 100);
+            //    graphics.DrawImage(resized, new Point(0, Convert.ToInt32((baseimg.Height / 2) - (resized.Height / 2))));
+            //    baseimg.Save(Path.GetDirectoryName(@"" + Environment.CurrentDirectory + "\\addwatermark\\") + "\\" + Path.GetFileNameWithoutExtension(img) + ".jpg");
+            //});
+            Console.WriteLine("Enter Water Mark Image Name");
+            string markName = Console.ReadLine();
+
+            Parallel.ForEach(Directory.GetFiles(@""+ Environment.CurrentDirectory + "\\img"),img =>
+            {
+                Image baseimg = Image.FromFile(img);
+                Graphics graphics = Graphics.FromImage(baseimg); 
+                Image watermark = Image.FromFile(@"" + Environment.CurrentDirectory + "\\watermark\\" + markName + ".png");
+                decimal de_w = (decimal)baseimg.Width / watermark.Width * 100;
+                int i_w = (int)de_w;
+                var resized = new Bitmap(watermark, (watermark.Width * i_w) / 100, (watermark.Height * i_w) / 100);
+                graphics.DrawImage(resized, new Point(0, Convert.ToInt32((baseimg.Height / 2) - (resized.Height / 2))));
+                baseimg.Save(Path.GetDirectoryName(@"" + Environment.CurrentDirectory + "\\addwatermark\\") + "\\" + Path.GetFileNameWithoutExtension(img) + ".jpg");
+            });
+        }
+    }
+}
---
+++ waterMark/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using addWaterMark;
+
+namespace waterMark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = Stopwatch.StartNew();
+
+            addMark addMark = new addMark();
+            addMark.watermark();
+
+            //var GetDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            //Console.WriteLine(GetDirectory);
+
+            Console.WriteLine("Parallel.ForEach() execution time = {0} seconds", stopWatch.Elapsed.TotalSeconds);
+            Console.ReadKey();
+        }
+    }
+}
