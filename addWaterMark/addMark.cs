e2c0fed55086e68024b155eac8fdc983bc37b668
shahnawaz.jarral984@gmail.com <shahnawaz.jarral984@gmail.com>
2021-09-06 21:31:09 +0500

    Agilers

--- addWaterMark/addMark.cs
+++ addWaterMark/addMark.cs
@@ -16,17 +16,7 @@ namespace addWaterMark
             Console.WriteLine("Enter Water Mark Image Name");
             string markName = Console.ReadLine();
 
-            Parallel.ForEach(Directory.GetFiles(@"" + Environment.CurrentDirectory + "\\img"), img =>
-            {
-                Image baseimg = Image.FromFile(img);
-                Graphics graphics = Graphics.FromImage(baseimg);
-                Image watermark = Image.FromFile(@"" + Environment.CurrentDirectory + "\\watermark\\" + markName + ".png");
-                decimal de_w = (decimal)baseimg.Width / watermark.Width * 100;
-                int i_w = (int)de_w;
-                var resized = new Bitmap(watermark, (watermark.Width * i_w) / 100, (watermark.Height * i_w) / 100);
-                graphics.DrawImage(resized, new Point(0, Convert.ToInt32((baseimg.Height / 2) - (resized.Height / 2))));
-                baseimg.Save(Path.GetDirectoryName(@"" + Environment.CurrentDirectory + "\\addwatermark\\") + "\\" + Path.GetFileNameWithoutExtension(img) + ".jpg");
-            });
+            
         }
     }
 }
